4f39ccdf7267fec75c688f324dcdc704ab083694
Ben Wilson <benwilson512@gmail.com>
2023-08-08 12:31:56 -0400

    toggle UI state based on running program

--- app/lib/coffee_time_web/live/programs/index.ex
+++ app/lib/coffee_time_web/live/programs/index.ex
@@ -16,23 +16,37 @@ defmodule CoffeeTimeWeb.Programs.Index do
   end
 
   @impl true
-  def handle_event("run", %{"program" => program}, socket) do
-    program = Enum.find(socket.assigns.programs, &(to_string(&1.name) == program))
+  def handle_event("program-click", %{"program" => program_name}, socket) do
+    %{
+      programs: programs,
+      context: context,
+      running_program: current
+    } = socket.assigns
 
-    if program do
-      :ok = CoffeeTime.Barista.run_program(socket.assigns.context, program)
+    if current && to_string(current.name) == program_name do
+      :ok = CoffeeTime.Barista.halt(context)
+
+      {:noreply, socket}
+    else
+      program = Enum.find(programs, &(to_string(&1.name) == program_name))
+
+      if program do
+        :ok = CoffeeTime.Barista.run_program(context, program)
+      end
+
+      # This is mildly redundant to the handle_info/2 clause
+      # that tracks the running program, but it helps avoid double
+      # pressing the button, and acts like an optimistic ui update
+      socket = assign(socket, :running_program, program)
+
+      {:noreply, socket}
     end
-
-    # This is mildly redundant to the handle_info/2 clause
-    # that tracks the running program, but it helps avoid double
-    # pressing the button, and acts like an optimistic ui update
-    socket = assign(socket, :running_program, program)
-
-    {:noreply, socket}
   end
 
   @impl true
   def handle_info({:broadcast, :barista, msg}, socket) do
+    msg |> dbg
+
     socket =
       case msg do
         {:program_start, program} ->
@@ -45,7 +59,26 @@ defmodule CoffeeTimeWeb.Programs.Index do
     {:noreply, socket}
   end
 
-  def format_name(program) do
+  def program_button(assigns) do
+    assigns =
+      assign(
+        assigns,
+        :current,
+        assigns.running_program && assigns.running_program.name == assigns.program.name
+      )
+
+    ~H"""
+    <.progress_button
+      phx-click={JS.push("program-click", value: %{program: @program.name})}
+      disabled={@running_program && !@current}
+    >
+      <.icon name={if @running_program && @current, do: "hero-pause-solid", else: "hero-play-solid"} />
+      <%= format_name(@program) %>
+    </.progress_button>
+    """
+  end
+
+  defp format_name(program) do
     program.name
     |> to_string
     |> String.split("_")
