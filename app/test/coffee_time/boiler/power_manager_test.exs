19a27ef7c1b456a6a99a68fc0fe9a55d76e94ed6
Ben Wilson <benwilson512@gmail.com>
2023-10-29 19:38:53 -0400

    idle tests

--- app/test/coffee_time/boiler/power_manager_test.exs
+++ app/test/coffee_time/boiler/power_manager_test.exs
@@ -3,13 +3,10 @@ defmodule CoffeeTime.Boiler.PowerManagerTest do
 
   import CoffeeTime.Application, only: [name: 2]
 
-  alias CoffeeTime.PubSub
-  alias CoffeeTime.Boiler.PowerControl
   alias CoffeeTime.Boiler.PowerManager
   alias CoffeeTime.Boiler.PowerManager.Config
   alias CoffeeTime.Boiler
   alias CoffeeTime.Measurement
-  alias CoffeeTime.Watchdog
 
   @moduletag :measurement_store
   @moduletag :watchdog
@@ -31,11 +28,61 @@ defmodule CoffeeTime.Boiler.PowerManagerTest do
     end
   end
 
-  def boot(%{context: context} = test_context) do
-    default_config = %{
-      idle_pressure: 0
-    }
+  describe "idle" do
+    setup :boot
 
+    @describetag config: %Config{
+                   idle_pressure: 10300,
+                   active_trigger_threshold: 9000,
+                   active_pressure: 12000
+                 }
+    test "pressure below the active trigger moves us to active", %{
+      context: context,
+      config: config
+    } do
+      # should be idle
+      assert {:idle, _} = :sys.get_state(name(context, Boiler.PowerManager))
+
+      # send some pressure below our trigger
+      Measurement.Store.put(context, :boiler_pressure, config.active_trigger_threshold - 1)
+
+      # should be idle
+      assert {:active, _} = :sys.get_state(name(context, Boiler.PowerManager))
+    end
+
+    test "pressure drop less than 100 does nothing", %{context: context} do
+      Measurement.Store.put(context, :boiler_pressure, 10300)
+
+      # should be idle
+      assert {:idle, %{prev_pressure: 10300}} = :sys.get_state(name(context, Boiler.PowerManager))
+
+      Measurement.Store.put(context, :boiler_pressure, 10250)
+
+      # should still be idle
+      assert {:idle, %{prev_pressure: 10250}} = :sys.get_state(name(context, Boiler.PowerManager))
+    end
+
+    test "pressure drop more than 100 should put us in active", %{
+      context: context,
+      config: %{active_pressure: active_target}
+    } do
+      Measurement.Store.put(context, :boiler_pressure, 10300)
+
+      # should be idle
+      assert {:idle, %{prev_pressure: 10300}} = :sys.get_state(name(context, Boiler.PowerManager))
+
+      Measurement.Store.put(context, :boiler_pressure, 10150)
+
+      assert {:active, %{prev_pressure: 10150}} =
+               :sys.get_state(name(context, Boiler.PowerManager))
+
+      # the power control should be set to our active value
+      assert {:hold_target, %{target: ^active_target}} =
+               :sys.get_state(name(context, Boiler.PowerControl))
+    end
+  end
+
+  def boot(%{context: context} = test_context) do
     config = test_context[:config] || %Config{}
 
     PowerManager.__set_config__(context, config)
